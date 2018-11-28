6c9c40cad0c8c6819b10c593708cb39b016e5cb6
VuBang <>
2018-11-28 13:35:55 +0700

    commit

--- dist/js/main.js
+++ dist/js/main.js
@@ -30,5 +30,58 @@ $(document).ready(function () {
     $('.item').addClass('underline');
     // $('.item:first-child').removeClass('underline')
     $('.item:last-child').removeClass('underline');
+    var a = $('.abc').height();
+    var b = a / 2;
+    // console.log(a, b)
+    $('.block:first-child .item').css("height", b);
+    $('.block:last-child .item').css("height", b);
 });
+
+// Main
+$(document).ready(function () {
+    getcountUp();
+});
+
+$(window).scroll(function () {
+    getcountUp();
+});
+
+$(window).resize(function () {
+    // getcountUp()
+});
+
+function getcountUp() {
+    $('[data-count]').each(function () {
+        var elm = $(this).offset().top;
+        var docS = $(window).scrollTop();
+        // console.log(elm,docS)
+        var docH = $(window).height();
+        // console.log(docH)
+        var num = $(this).attr('data-count');
+        var f = $(this).attr('data-from');
+        var run = false;
+        if (elm < docS + docH - 400 && !run) {
+            // console.log(docH,docS, num)
+            // $(this).text(num)
+            var $this = $(this),
+                countTo = num,
+                durationTo = 30000;
+            $({ countNum: $this.text() }).animate({
+                countNum: countTo
+            }, {
+                duration: durationTo,
+                easing: 'linear',
+                step: function () {
+                    $this.text(Math.floor(this.countNum));
+                },
+                complete: function () {
+                    $this.text(this.countNum);
+                    run = true;
+                }
+            });
+        } else {
+            $(this).text(f);
+        }
+    });
+}
 //# sourceMappingURL=main.js.map
